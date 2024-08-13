94073527b6323e42b5455ea06d027be2f072937f
Chewbacca <higgimat000@warren.k12.in.us>
2024-08-13 13:45:03 -0400

    initial commit

---
+++ src/pnz.java
@@ -0,0 +1,18 @@
+public class pnz {
+    public static void main(String[] args) {
+        checkNumber(2);
+        checkNumber(-3);
+        checkNumber(0);
+    }
+
+    public static void checkNumber(int number) {
+        if (number > 0) {
+            System.out.println("The number is positive.");
+        } else if (number < 0) {
+            System.out.println("The number is negative");
+        }else {
+            System.out.println("The number is zero");
+        }
+    }
+
+}
